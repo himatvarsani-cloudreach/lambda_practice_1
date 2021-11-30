5624c4f0cac231afadc799bdf92acffab7812d23
himatvarsani-cloudreach <himat.varsani@cloudreach.com>
2021-11-30 18:00:05 +0000

    added code to index.py

--- backend.tf
+++ backend.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+      #copy the url from the aws cli - bucket
+    bucket = "lambda-exercise-139446191400-tfstates"
+    key    = "sprint2/week4/lambda/terraform.tfstates"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
 # Creating IAM role so that Lambda service to assume the role and access other AWS services. 
 
 resource "aws_iam_role" "lambda_role" {
