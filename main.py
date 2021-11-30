b56064f98dba82c91f654641b92da6c185b21aab
himatvarsani-cloudreach <himat.varsani@cloudreach.com>
2021-11-30 15:48:33 +0000

    Inititial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# import boto3
+# import json
+
+# s3_client = boto3.client('s3')
+
+# response = s3_client.get_object(Bucket='my-bucket-139446191400', Key='sample-data.json')
+
+# data = response['Body'].read()
+# print(data)
